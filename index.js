64e74bc6e04fb0230be57c9e13fd9718f2f1d550
shukronaasqarova <shukronaasqarova94@gmail.com>
2024-08-07 14:18:45 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,241 @@
+// 1-masala
+
+// let arr = [1,2,3,4,5,6];
+// function toqSonlar(arg){
+//     let counter = 0;
+//     for(let i = 0; i < arg.length; i++){
+//         if(arg[i] % 2 == 1){
+//             counter++;
+//         }
+//     }
+//     return counter;
+// }
+// console.log(toqSonlar(arr));
+
+// 2-masala
+
+// let str = "sa2lom1";
+// function faqatSonlar(s){
+//     let res = [];
+//     for (let i = 0; i < s.length; i++) {
+//         if(s[i] > 0){
+//             res.push(s[i])
+//         }
+//     }
+//     return res
+// }
+// console.log(faqatSonlar(str));
+
+// 3-masala
+
+// let str = "salom bolalar";
+// function sozlarSoni(s){
+//     let counter = 0;
+//     let m = s.split(' ');
+//     for (const iterator of m) {
+//         counter++
+//     }
+//     return counter
+// }
+// console.log(sozlarSoni(str));
+
+// 4-masala
+
+// let arr = [5,8,9,6,4,2,7];
+// function engKichik(arg){
+//     let res = arr.sort(function (a,b){
+//         return a - b
+//     })
+//     return res[0]
+// }
+// console.log(engKichik(arr));
+
+// 5-masala
+
+// let str = "salom";
+// function teskariSoz(s){
+//     return s.split('').reverse().join("")
+// }
+// console.log(teskariSoz(str));
+
+// 6-masala
+
+// let num = 45;
+// function raqamlarKopaytmasi(a){
+//     let m = a.toString().split('');
+//        let p = 1;
+//     for (const iterator of m) {
+//         p = p * iterator
+//     }
+//     return p
+// }
+// console.log(raqamlarKopaytmasi(num));
+
+// 7-masala
+
+// let arr = [1,2,3,4,5,6];
+// function kattadanKichikka(arg){
+//     let res = arg.sort(function(a,b){
+//         return b - a
+//     })
+//     return res
+// }
+// console.log(kattadanKichikka(arr));
+
+// 8-masala
+
+// let str = "salom bolalar";
+// function sozniOchir(s){
+//     let res = s.replace('salom'," ")
+//     return res
+// }
+// console.log(sozniOchir(str));
+
+// 9-masala
+
+// let arr = ["salom", "bolalar"];
+// function kattaHarflar(arg){
+//     return arg.join(' ').toUpperCase();
+// }
+// console.log(kattaHarflar(arr));
+
+// 10-masala
+
+// let arr = [1,2,3,4,5,6];
+// function toqSonlar(arg){
+//     let res = []
+//     for (let i = 0; i < arg.length; i++){
+//         if(arg[i] % 2 == 1){
+//             res.push(arg[i])
+//         }
+//     }
+
+//     return res
+// }
+// console.log(toqSonlar(arr));
+
+// 11-masala
+
+// let arr = ["salom", "dunyooooo"];
+// function uzunSoz(a){
+//     let m = a[0]
+//     for(let i = 0; i < a.length; i++){
+//         if(a[i].length > m.length){
+//             m = a[i]
+//         }
+//     }
+//     return m
+// }
+// console.log(uzunSoz(arr));
+
+// 12-masala
+
+// let arr = [1,2,3,4,5,6];
+// function yigindi(arg){
+//     let sum = 0
+//     for (const iterator of arg) {
+//         sum+= iterator
+//     }
+//     return sum
+// }
+// console.log(yigindi(arr));
+
+// 13-masala
+
+// let arr = [1,-2,3,-4,5,-6];
+// function musbatSonlar(arg){
+//    let res = arg.filter(function(v){
+//     return v > 0
+// })
+// return res
+// }
+// console.log(musbatSonlar(arr));
+
+// 14-masala
+
+// let arr = ['salom','dunyo'];
+// function sozlarUZunligi (arg) {
+//     let res = [];
+//     for (const iterator of arg) {
+//         res.push(iterator.length)
+//     }
+//     return res
+// }
+// console.log(sozlarUZunligi(arr));
+
+// 15-masala
+
+// let arr = [1,2,3,4,5];
+// function kopaytma(arg){
+//     let p = 1;
+//     for (const iterator of arg) {
+//         p = p * iterator
+//     }
+//     return p
+// }
+// console.log(kopaytma(arr));
+
+// 16-masala
+
+// 17-masala
+
+// let arr = [1,2,3,4,5,6];
+// function teskariArr(arg){
+//     let res = []
+//     for (const iterator of arg) {
+//         res.unshift(iterator)
+//     }
+//     return res
+// }
+// console.log(teskariArr(arr));
+
+// 18-masala
+
+// let arr = ['Anora','Malika','anvar','Fotima'];
+// function alfboTartib(arg){
+//     return arg.sort()
+// }
+// console.log(alfboTartib(arr));
+
+// 19-masala
+
+// let str = "salom bolalar";
+// function alifbotartib(a){
+//     return a.split('').sort().join(' ')
+// }
+// console.log(alifbotartib(str));
+
+// 20-masala tugamadi
+
+// let str = 'salom bolalar';
+// function kattaHarf(a){
+//     return a.
+// }
+
+// 21-masala
+
+// let arr = "salom dunyo";
+// function test(arg) {
+//   let res = arg.split(" ").join("");
+//   return res;
+// }
+// let res = test(arr);
+// console.log(res);
+
+// 22-masala
+
+
+
+// 23-masala
+
+// let str = 'salom dunyo';
+// function sozlarUzunligi(s) {
+//     let res = [];
+//     let words = s.split(' '); 
+//     for (const iterator of words) {
+//         res.push(iterator.length); 
+//     }
+//     return res;
+// }
+
+// console.log(sozlarUzunligi(str)); 
